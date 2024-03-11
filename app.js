0b1b0fbd0664a4546ebc1142a1e715a9071c8dc4
whailey7724 <155971128+whailey7724@users.noreply.github.com>
2024-03-11 18:30:32 +0900

    Loading Username
    if문으로 조건을 추가해서 자동으로 form과 h1을 숨기게끔 함..
    반복되는 두 줄은 paintGreetings 함수를 사용해서 없애버림!

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "usesrname";   // 변수가 아니라 string
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);    // 저장될 아이템의 이름, 값은 username변수
-    greeting.innerText = `Hello ${username} !!*^^*`;
+    localStorage.setItem(USERNAME_KEY, username);    // 저장될 아이템의 이름, 값은 username변수
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${savedUsername} !!*^^*`;   
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUsername);
+
+if(savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greeting
+    paintGreetings(savedUsername);      // 여기서는 유저정보가 local storage로부터 오고 있음. 위에(username 사용한 곳)는 input으로부터 오고있음
+}
