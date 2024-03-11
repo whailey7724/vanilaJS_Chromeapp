b5580c20d5a26adf5b1a07094f3c47fa76a74da1
whailey7724 <155971128+whailey7724@users.noreply.github.com>
2024-03-11 18:39:41 +0900

    paintGreeting 함수에 매개변수 안받고 하는 경우
    local storage에 무언가를 저장하면 paintGreetings를 호출하는 순간에 그 유저정보는 이미 local storage에 저장되어있음 _변수를 받아올 필요가 없음 --- local storgae 두 번 열어보는 방식

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const USERNAME_KEY = "usesrname";   // 변수가 아니라 string
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);    // 저장될 아이템의 이름, 값은 username변수
-    paintGreetings(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);    // 저장될 아이템의 이름, 값은 username변수
+    paintGreetings();
 }
 
-function paintGreetings(username) {
-    greeting.innerText = `Hello ${savedUsername} !!*^^*`;   
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);   
+    greeting.innerText = `Hello ${username} !!*^^*`;   
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -28,5 +28,5 @@ if(savedUsername === null) {
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     // show the greeting
-    paintGreetings(savedUsername);      // 여기서는 유저정보가 local storage로부터 오고 있음. 위에(username 사용한 곳)는 input으로부터 오고있음
+    paintGreetings();      // 여기서는 유저정보가 local storage로부터 오고 있음. 위에(username 사용한 곳)는 input으로부터 오고있음
 }
