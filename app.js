8798b3cafba9190db840480d513ebb13ae0811d9
whailey7724 <155971128+whailey7724@users.noreply.github.com>
2024-03-11 19:22:14 +0900

    4.LOGIN끝
    원래대로돌려놓기
    (바로전 커밋 이전버전)

--- app.js
+++ app.js
@@ -8,19 +8,17 @@ const USERNAME_KEY = "usesrname";   // 변수가 아니라 string
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem(USERNAME_KEY, loginInput.value);    // 저장될 아이템의 이름, 값은 username변수
-    paintGreetings();
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);    // 저장될 아이템의 이름, 값은 username변수
+    paintGreetings(username);
 }
 
-function paintGreetings() {
-    const username = localStorage.getItem(USERNAME_KEY);   
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username} !!*^^*`;   
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-console.log(savedUsername);
 
 if(savedUsername === null) {
     // show the form
@@ -28,5 +26,5 @@ if(savedUsername === null) {
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     // show the greeting
-    paintGreetings();      // 여기서는 유저정보가 local storage로부터 오고 있음. 위에(username 사용한 곳)는 input으로부터 오고있음
+    paintGreetings(savedUsername);      // 여기서는 유저정보가 local storage로부터 오고 있음. 위에(username 사용한 곳)는 input으로부터 오고있음
 }
