740551e0315dc80ebd8feb45deaf366055b0c8df
whailey7724 <155971128+whailey7724@users.noreply.github.com>
2024-03-30 20:57:46 +0900

    localStorage에 저장하기
    할일 목록을 localStorage에 입력함
    - localStorage는 array는 저장할 수 없고 텍스트만 저장할 수 있음 >> 어떤 코드든 string으로 만들어주는 웹사이트의 기능.. JSON을 이용해서 array 형식으로 저장함

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,15 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input"); >> 위에랑 똑같은 코드
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+// localStorage에는 배열을 저장할 수 없음.. 텍스트만 저장할 수 있음
+// JSON.stringify는 JavaScript object나 array 또는 어떤 JS 코드건 간에 그걸 string으로 만들어줌.
+// 그전에는 그냥 텍스트로 저장되었지만, JSON.stringify를 이용해서 array처럼 보이게끔 저장하게 됨. ( ["a", "b", "c"] 이런 식으로 )
+
 function deleteToDo(event) {
     // eventlistener을 추가해서 버튼을 클릭한 걸 확인하게 만들었지만 문제가 있음 >> 어떤 버튼을 클릭했는지 모름
     // console.log(event.target.parentElement);    // 목록중에 어떤 버튼을 클릭했는지 확인할 수 있음 / parentElement는 클릭된 element의 부모
@@ -27,7 +36,9 @@ function handleTodoSubmit() {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
